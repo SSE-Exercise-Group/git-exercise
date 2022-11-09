cc3456c7e149186a3491380ce84c9900e2fd1d78
Andreas Farley <st141408@stud.uni-stuttgart.de>
2022-11-09 13:00:28 +0100

    Added interval function to solve duplicate problem (#6)
    Co-authored-by: aburabazam <76391772+aburabazam@users.noreply.github.com>

--- exercise3.py
+++ exercise3.py
@@ -26,13 +26,11 @@ class Rectangle:
         return self.corner(3)
 
     def contains(self, point: Point2D, tolerance=0.) -> bool:
-        # Task A: remove duplication by defining a function
-        #         that checks if a value is within an interval
-        #         and reuse that here.
         ll_px = point.x - self._lower_left.x
         ll_py = point.y - self._lower_left.y
-        return ll_px >= - tolerance and ll_px <= self._dx + tolerance \
-            and ll_py >= - tolerance and ll_py <= self._dy + tolerance
+        return is_within_interval(ll_px, self._dx, tolerance) \
+            and is_within_interval(ll_py, self._dy, tolerance)
+
 
     def _is_idx_on_upper_edge(self, i: int) -> bool:
         return i in [2, 3]
@@ -40,12 +38,13 @@ class Rectangle:
     def _is_idx_on_right_edge(self, i: int) -> bool:
         return i in [1, 3]
 
+def is_within_interval(x: int, interval: int):
+    return x >= - tolerance and x <= interval + tolerance
 
 def test_rectangle_contains_exact() -> None:
     rectangle = Rectangle(lower_left=Point2D(1.0, 2.0), dx=2.5, dy=1.5)
     for i in range(4):
         assert rectangle.contains(rectangle.corner(i))
-
 
 def test_rectangle_contains_tolerance() -> None:
     rectangle = Rectangle(lower_left=Point2D(1.0, 2.0), dx=2.5, dy=1.5)
